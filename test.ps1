180c55052b4b2e7b721bc39093b8c923da80fd79
Capmare Alex-Mihai <73711932+CapmareAlex@users.noreply.github.com>
2023-02-09 10:48:09 +0200

    Update test.ps1

--- test.ps1
+++ test.ps1
@@ -1,1 +1,19 @@
 $PSVersionTable
+Disable-AzContextAutosave -Scope Process
+$AzureContext = (Connect-AzAccount -Identity).context
+$AzureContext = Set-AzContext -SubscriptionName $AzureContext.Subscription -DefaultProfile $AzureContext
+ 
+
+
+$vault_keys = Get-AzKeyVault
+
+foreach ($key_vault in $vault_keys)
+{    
+    
+    $keys = Get-AzKeyVaultKey -VaultName $key_vault.VaultName
+
+    foreach( $key in $keys)
+    {
+        Write-Output ($key )
+    }
+}
